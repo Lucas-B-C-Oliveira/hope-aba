721ce29d62193196b0aff54c2ec04a281c789bce
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-09-30 15:03:55 -0400

    refactor: move functions to helper.ts

--- src/utils/functions/helpers.ts
+++ src/utils/functions/helpers.ts
@@ -1,3 +1,5 @@
+import { Filter } from "@/types"
+
 export function tokenDecode(token: string | undefined | null) {
   if (!token) {
     return null
@@ -11,3 +13,36 @@ export function tokenDecode(token: string | undefined | null) {
 export function capitalizeFirstLetter(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+
+export function makeQuery(queryKey: string, value: Filter | Filter[] | undefined) {
+  if (Array.isArray(value) && value.length > 0) {
+    const ids = value.map((val: { id: string; name: string }) => val?.id)
+    const values = ids.join(',')
+    return `&${queryKey}=${values}`
+  } else if (!Array.isArray(value) && typeof value?.id === 'string') {
+    return `&${queryKey}=${value?.id}`
+  }
+  return `__`
+}
+
+export function makeQueryByArray(queryKey: string, value: string[] | undefined) {
+  if (Array.isArray(value) && value.length > 0) {
+    const ids = value.map((val: string) => val)
+    const values = ids.join(',')
+    return `&${queryKey}=${values}`
+  }
+
+  return `__`
+}
+
+
+export function removeSpacesOfString(query: string) {
+  return query.replace(/__/g, '')
+}
+
+export function removeFirstCharacter(query: string) {
+  return query.substring(1)
+}
+
+
