22909623d0df4564ad09ae2ec4e244562b9c9b19
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-09 18:32:06 -0400

    refactor: remove useless code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,18 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 import { ACCESS_TOKEN, CLINICS_DATA } from './utils/functions/constants'
 
-export async function middleware(request: NextResponse) {
-  // console.log('FORA request.url ', request.url)
+export async function middleware(request: NextRequest) {
 
   const accessToken = request.cookies.get(ACCESS_TOKEN)
-  const clinicsData = request.cookies.get(CLINICS_DATA)
-
-  const authenticatedUser =
-    typeof accessToken?.value === 'string' &&
-    typeof clinicsData?.value === 'string'
-
-  // console.log(ACCESS_TOKEN, accessToken)
-  // console.log(CLINICS_DATA, clinicsData)
+  const cookieClinicsData = request.cookies.get(CLINICS_DATA)
+  const authenticatedUser = typeof accessToken?.value === 'string' && typeof cookieClinicsData?.value === 'string'
 
   if (!authenticatedUser) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
+
+
   return null
 }
 
