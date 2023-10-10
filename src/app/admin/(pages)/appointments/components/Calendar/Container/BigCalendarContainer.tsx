2f59aaa1bc1ec5f6c39fa46fbd390d3a455125f6
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-10 17:13:29 -0400

    feat: remove server action

--- src/app/admin/(pages)/appointments/components/Calendar/Container/BigCalendarContainer.tsx
+++ src/app/admin/(pages)/appointments/components/Calendar/Container/BigCalendarContainer.tsx
@@ -4,7 +4,6 @@ import { memo, useEffect, useRef, useState } from 'react'
 import { dateAdapter } from '@/utils/dateAdapter'
 import { Calendar } from '..'
 import {
-  getAppointmentsByRangeDate,
   getProfessionalScheduleAvailability,
 } from '@/utils/actions/action'
 import { useAppointmentFilterStore } from '@/store/appointmentFilterStore'
@@ -19,12 +18,61 @@ import {
   removeSpacesOfString,
   tokenDecode,
 } from '@/utils/functions/helpers'
+import { CSFetch } from '@/utils/api/clientFetch'
 
 interface AppointmentCard {
   start: Date
   end: Date
   title?: string
   data: any
+}
+
+interface Response {
+  data?: any[]
+}
+
+export async function getAppointmentsByRangeDate<T = unknown>(
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined | any,
+) {
+  try {
+    const response = (await CSFetch<T>(input, init)) as Response
+    const cardsAppointment = response?.data
+      ? response?.data.map((appointmentData: any) => {
+        const { day, start, end } = appointmentData.schedule
+
+        const { patient, therapy } = appointmentData
+
+        const patientNameSplited = patient.name.split(' ')
+
+        const patientNameLabel = `${patientNameSplited[0]} ${patientNameSplited[patientNameSplited.length - 1]
+          }` //! TODO: Format this name to First upercase first lether
+
+        //   const startDate = dateAdapter(`${day}T${start}:00`).local().toDate()
+        //   const endDate = dateAdapter(`${day}T${end}:00`).local().toDate()
+
+        //   const startDate = new Date(`${day}T${start}:00Z`)
+        // const endDate = new Date(`${day}T${end}:00Z`)
+
+        const startDate = `${day}T${start}:00`
+        const endDate = `${day}T${end}:00`
+
+        return {
+          start: startDate,
+          end: endDate,
+          data: {
+            ...appointmentData,
+            patientNameLabel,
+            therapyNameLabel: therapy?.name,
+          },
+        }
+      })
+      : []
+
+    return cardsAppointment
+  } catch (error: unknown | string | undefined) {
+    throw new Error(`${error}`)
+  }
 }
 
 const components = {
@@ -203,11 +251,14 @@ export const BigCalendarContainer = memo(function BigCalendarContainer() {
         }
       }
       console.log(
-        '______ endpointWithQuery',
+        'query',
         endpointWithQuery + '&page=1&pageSize=90',
       )
       const data = await getAppointmentsByRangeDate(
         endpointWithQuery + '&page=1&pageSize=90',
+        {
+          cache: 'no-store'
+        }
       )
 
       const newData = data.map((data) => {
@@ -243,8 +294,14 @@ export const BigCalendarContainer = memo(function BigCalendarContainer() {
   }, [])
 
   useEffect(() => {
+    console.log('useEffect que verifica o filterButtonStatusAppointment', filterButtonStatusAppointment)
     if (filterButtonStatusAppointment !== 'idle') {
+      console.log('currentCalendarWeekday.current', currentCalendarWeekday.current)
       makeAppointmentFeedback(currentCalendarWeekday.current)
+    }
+
+    if (filterButtonStatusAppointment === 'clicked') {
+      setButtonStatusAppointment('idle')
     }
   }, [filterButtonStatusAppointment])
 
