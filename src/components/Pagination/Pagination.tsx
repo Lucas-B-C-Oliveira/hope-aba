a7465bb01918e177f85a8c7e7570c2f75d4567ad
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-10 20:18:10 -0400

    feat: add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Meta } from "@/types"
+import { PaginationItem } from "./PaginationItem"
+import { usePagination } from "./usePagination"
+
+
+interface Props {
+  meta: Meta
+}
+
+export function Pagination({ meta }: Props) {
+
+  const { currentPage, pagesArray } = usePagination({ meta })
+
+  return (
+
+    <div className="flex flex-row gap-1 items-center p-1">
+      {pagesArray && pagesArray.length > 0 && pagesArray.map((item: number) => {
+
+        return (
+          <PaginationItem newPage={item} isSelected={item === currentPage} />
+        )
+      })}
+    </div>
+  )
+}
