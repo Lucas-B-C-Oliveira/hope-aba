732c9491715829042d86dd6082d5aa0b5ae3ad8f
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-23 02:11:59 -0400

    refactor: remove fragment

--- src/app/admin/(pages)/appointments/components/Calendar/Container/Toolbar.tsx
+++ src/app/admin/(pages)/appointments/components/Calendar/Container/Toolbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { OpenModalButton } from '@/components/OpenModalButton'
 import { Calendar } from '..'
 import { useToolbar } from './useToolbar'
@@ -18,26 +19,26 @@ export function Toolbar(props: any) {
     openAndCloseLeftContentSidebar(!leftContentSidebarIsOpen)
   }
 
+  console.log('######### toolbar Props', props)
+
   return (
-    <>
-      <Calendar.Header
-        openLeftContentSidebarButton={
-          <OpenModalButton onClick={openAndCloseLeftContentSidebarHandle}>
-            {!leftContentSidebarIsOpen && 'Filtros'}
-            {leftContentSidebarIsOpen && 'Fechar'}
-          </OpenModalButton>
-        }
-        calendarDate={props.date}
-        onNavigatePrev={() => props.onNavigate('PREV')}
-        onNavigateNext={() => props.onNavigate('NEXT')}
-        onNavigateToday={() => props.onNavigate('TODAY')}
-        openRightContentSidebarButton={
-          <OpenModalButton onClick={openAndCloseRightContentSidebarHandle}>
-            {!rightContentSidebarIsOpen && 'Agendar'}
-            {rightContentSidebarIsOpen && 'Fechar'}
-          </OpenModalButton>
-        }
-      />
-    </>
+    <Calendar.Header
+      openLeftContentSidebarButton={
+        <OpenModalButton onClick={openAndCloseLeftContentSidebarHandle}>
+          {!leftContentSidebarIsOpen && 'Filtros'}
+          {leftContentSidebarIsOpen && 'Fechar'}
+        </OpenModalButton>
+      }
+      calendarDate={props.date}
+      onNavigatePrev={() => props.onNavigate('PREV')}
+      onNavigateNext={() => props.onNavigate('NEXT')}
+      onNavigateToday={() => props.onNavigate('TODAY')}
+      openRightContentSidebarButton={
+        <OpenModalButton onClick={openAndCloseRightContentSidebarHandle}>
+          {!rightContentSidebarIsOpen && 'Agendar'}
+          {rightContentSidebarIsOpen && 'Fechar'}
+        </OpenModalButton>
+      }
+    />
   )
 }
