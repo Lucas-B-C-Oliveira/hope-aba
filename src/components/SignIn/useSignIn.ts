b4f8ba5ec2aa5e278e5ee7bd6d8407212287bafc
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-31 14:02:14 -0400

    fix: remove await to avoid frize system when password is wrong

--- src/components/SignIn/useSignIn.ts
+++ src/components/SignIn/useSignIn.ts
@@ -109,7 +109,7 @@ export function useSignIn() {
   } = signInForm
 
   async function handleSignIn(data: SignInData) {
-    await signInMutate(data)
+    signInMutate(data)
   }
 
   function handleGoToAppointments() {
