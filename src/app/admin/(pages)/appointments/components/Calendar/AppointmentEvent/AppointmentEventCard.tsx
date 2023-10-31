b0b3a111861058501a98becd1cb99c67a768fe87
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-31 15:44:43 -0400

    style: add color by status color

--- src/app/admin/(pages)/appointments/components/Calendar/AppointmentEvent/AppointmentEventCard.tsx
+++ src/app/admin/(pages)/appointments/components/Calendar/AppointmentEvent/AppointmentEventCard.tsx
@@ -1,24 +1,37 @@
 import { HTMLProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface Props extends HTMLProps<HTMLDivElement> {
   patientNameLabel: string
   therapyNameLabel: string
+  status: 'canceled' | 'confirmed' | 'done' | 'pending'
+  statusLabel?: string
+  classNameToMerge?: string
 }
 
 export function AppointmentEventCard({
   patientNameLabel,
   therapyNameLabel,
+  status,
+  statusLabel,
+  classNameToMerge,
   ...rest
 }: Props) {
+  console.log('patientNameLabel', patientNameLabel)
   return (
     <div
       {...rest}
       className=" sm:flex h-full w-full"
       style={{ paddingLeft: '4px', paddingRight: '4px' }}
     >
-      <a className="border-gray-100 border-2 w-full h-full  overflow-y-auto flex flex-col rounded-lg bg-yellow-300 px-2 py-1 text-xs leading-4 hover:bg-yellow-400">
-        <p className="font-semibold text-gray-700">{patientNameLabel}</p>
-        <p className="font-semibold text-gray-700">{therapyNameLabel}</p>
+      <a
+        className={twMerge(
+          'border-gray-100 border-2 w-full h-full  flex flex-col rounded-lg bg-yellow-500 px-2 py-1 text-xs leading-4 hover:bg-yellow-400 overflow-hidden',
+          classNameToMerge,
+        )}
+      >
+        <p className="text-xs font-bold text-white">{patientNameLabel}</p>
+        <p className="font-bold text-gray-700">{therapyNameLabel}</p>
       </a>
     </div>
   )
