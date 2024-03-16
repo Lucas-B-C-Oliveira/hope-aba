efe646958fde8756de64525e6300c259fd7ba808
Lucas Oliveira <lucascoliveira819@gmail.com>
2024-03-16 16:45:58 -0400

    refactor: remove useless code

--- src/app/admin/(pages)/appointments/@leftcontentsidebar/components/Filter/useCheckboxesTherapiesFilters.ts
+++ src/app/admin/(pages)/appointments/@leftcontentsidebar/components/Filter/useCheckboxesTherapiesFilters.ts
@@ -21,7 +21,7 @@ export function useCheckboxesTherapiesFilters(
   const [checkboxes, setCheckboxes] = useState<any[] | []>([])
   const { addFilter, removeFilter } = useAppointmentFilterStore()
 
-  const { setValue, getValues, control } = useFormContext()
+  const { setValue, getValues } = useFormContext()
 
   const formValues = getValues()
 
