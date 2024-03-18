268907007846343bdb211653935cd5514523ceb6
Lucas Oliveira <lucascoliveira819@gmail.com>
2024-03-18 10:18:37 -0400

    refactor: remove useless code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,11 +52,6 @@ export function Header() {
 
           <Menu as="div" className="relative">
             <Menu.Button className="-m-1.5 flex items-center p-1.5">
-              {/* <img
-                className="h-8 w-8 rounded-full bg-gray-50"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              /> */}
               <span className="hidden lg:flex lg:items-center">
                 <span
                   className="ml-4 text-sm font-semibold leading-6 text-gray-900"
